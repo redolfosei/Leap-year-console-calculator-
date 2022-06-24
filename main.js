6ea21a57dd41cd018938830508a5354d8178dc2b
redolf <redolf639@gmail.com>
2022-06-24 22:42:45 +0000

    initial commit

---
+++ main.js
@@ -0,0 +1,10 @@
+// The Leap Year calculator '@roktech | redolF' 
+function isLeap(year) {
+    var four = 4;
+    var hundred = 100;
+    var four_hundred = 400;
+
+    if (year%four === 0) {
+        
+    }
+}
