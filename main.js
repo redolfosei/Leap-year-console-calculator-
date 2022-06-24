5712d02ebc434c6987e5cb5ec99bf7cbe98c8d30
redolf <redolf639@gmail.com>
2022-06-24 23:34:11 +0000

    basic leap year console calculator done

--- main.js
+++ main.js
@@ -4,7 +4,19 @@ function isLeap(year) {
     var hundred = 100;
     var four_hundred = 400;
 
-    if (year%four === 0) {
-        
+    if (year % four === 0) {
+        if (year % hundred === 0){
+            if (year % four_hundred === 0) {
+                return "Leap year";
+            } else {
+                return "Not Leap year."
+            }
+        }else {
+            return "Leap year."
+        }
+    } else {
+        return "Not leap year.";
     }
 }
+
+console.log(isLeap(2023));
